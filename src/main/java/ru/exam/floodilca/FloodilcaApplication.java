2539d20c9228bfd3439abc190f276ac7bcc61e45
Yakov Dell <yakov.radaev@ya.ru>
2022-12-01 20:28:41 +0300

    first commit

---
+++ src/main/java/ru/exam/floodilca/FloodilcaApplication.java
@@ -0,0 +1,13 @@
+package ru.exam.floodilca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloodilcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloodilcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/exam/floodilca/FloodilcaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.exam.floodilca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloodilcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
