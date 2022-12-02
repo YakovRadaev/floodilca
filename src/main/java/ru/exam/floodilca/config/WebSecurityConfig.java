2de1ca98c3adf5677f17fbb9f6137b033c16ca2f
Yakov Dell <yakov.radaev@ya.ru>
2022-12-02 20:36:35 +0300

    first commit
    БД
    
    Security и регистрация польз в БД
    
    Связи между таблицами
    
    Панель администратора

--- src/main/java/ru/exam/floodilca/config/WebSecurityConfig.java
+++ src/main/java/ru/exam/floodilca/config/WebSecurityConfig.java
@@ -1,26 +1,18 @@
 package ru.exam.floodilca.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import ru.exam.floodilca.service.UserService;
-
-import javax.sql.DataSource;
-
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//    @Autowired
-//    private DataSource dataSource;
 
     @Autowired
     private UserService userService;
@@ -40,28 +32,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll();
     }
 
-//    @Bean
-//    @Override
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user =
-//                User.withDefaultPasswordEncoder()
-//                        .username("user")
-//                        .password("password")
-//                        .roles("USER")
-//                        .build();
-//
-//        return new InMemoryUserDetailsManager(user);
-//    }
-
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .userDetailsService(userService)
-//                .jdbcAuthentication()
-//                .dataSource(dataSource)
                 .passwordEncoder(NoOpPasswordEncoder.getInstance());
-//                .usersByUsernameQuery("select username, password, active from usr where username=?")
-//                .authoritiesByUsernameQuery("select u.username, ur.roles from usr u inner join user_role ur on u.id = ur.user_id where u.username=?");
     }
 }
--- src/main/java/ru/exam/floodilca/domain/Role.java
+++ src/main/java/ru/exam/floodilca/domain/Role.java
@@ -3,7 +3,7 @@ package ru.exam.floodilca.domain;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER, ADMIN;
 
     @Override
     public String getAuthority() {
